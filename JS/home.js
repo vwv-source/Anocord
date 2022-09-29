ce8980fd2ac30315e607bc77180ca74283b734b6
vwv-source <abdullahmohfathy@gmail.com>
2022-09-29 02:52:18 +0200

    new stuffs

--- JS/home.js
+++ JS/home.js
@@ -1,8 +1,8 @@
 import 'https://code.jquery.com/jquery-3.6.0.min.js'
 
 //----------Firebase-------------
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js";
-import { getDatabase, ref, set, push, update, get, remove } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getDatabase, ref, set, push, update, get, remove } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyATBGPrXWawfFQJIAdTk0MD7fIJvGmEteU",
@@ -21,25 +21,30 @@ var selectedchannel = 'general';
 var shiftpressed;
 var profileopen = false;
 var settingsopen = false;
+var currentsettingmenu;
 
 setInterval(function(){ 
     const messages = ref(getDatabase(), selectedchannel)
     get(messages).then((snapshot) => {
-        $('.messagecontainer').empty();
         var snapshot = snapshot.val();
-        for(let index in snapshot){
-            var element = snapshot[index];
-            $('.messagecontainer').append(`<div class="message"> <div class="userprofileimg"></div><p class="username">vewu</p><p class="time">15:00</p><br><br><p class="messagetext">${element}</p></div>`)
+        if(!snapshot){
+            return;
+        }else if($('.messagecontainer').children().length == snapshot.length || !snapshot){
+            return;
+        }else{
+            $('.messagecontainer').empty();
+            for(let index in snapshot){
+                var element = snapshot[index];
+                $('.messagecontainer').append(`<div class="message"> <div class="userprofileimg"></div><p class="username">vewu</p><p class="time">15:00</p><br><br><p class="messagetext">${element}</p></div>`)
+            }
         }
     })
 }, 200);
 
 $(document).on('click', '.categoryname', function(e){
     if($(this).attr('hide') == ""){
-        console.log('hello')
         $(this).parent().children().removeAttr('hide')
     }else{
-        console.log('hell')
         $(this).parent().children().attr('hide', '')
     }
 })
@@ -65,6 +70,7 @@ $(document).on('click', '.miniprofile' ,function(e){
 
 $(document).on('click', function(e){
     if(settingsopen == true && $(e.target).is(".settingscontainer")){
+        currentsettingmenu = null;
         settingsopen = false;
         $(".settingscontainer").remove()
     }else if(profileopen == true && $(e.target).is(".chatcontainer")){
@@ -81,6 +87,7 @@ $(document).on('click', function(e){
 
 $('.channelbutton').on('click', function(e){
     e.preventDefault()
+    $('.messagecontainer').empty();
     $('.channelbutton[selected]').removeAttr('selected')
     $(this).attr('selected', '');
     selectedchannel = $(this).attr('channelid')
@@ -101,24 +108,81 @@ $("#chattextinput").on('keydown', async function (e) {
     }else
     if (e.key == 'Enter' || e.keyCode == 13) {
         e.preventDefault();
+        var image;
+        if ($('.imageupbutton').prop("files") && $('.imageupbutton').prop("files")[0]) {
+            var reader = new FileReader();
+            reader.onloadend = function() {
+                image = reader.result;
+                console.log(image);
+            }
+            reader.readAsDataURL($('.imageupbutton').prop("files")[0]);
+        }
         const messages = ref(getDatabase(), selectedchannel)
         const chat = ref(getDatabase(), selectedchannel)
-        update(chat, 
-        {
-            [
-                await get(messages).then((snapshot) => {if(!snapshot.val()){return 0}return snapshot.val().length})
-            ]:$('#chattextinput').val()
-        })
-        setTimeout(function(){$('.chatcontainer').scrollTop($('.chatcontainer')[0].scrollHeight);}, 500);
+        update(chat,
+            {
+                [
+                    await get(messages).then((snapshot) => { if (!snapshot.val()) { return 0 } return snapshot.val().length })
+                ]: `${$('#chattextinput').val()} <br><img src="${image}">`
+            })
+        setTimeout(function () { $('.chatcontainer').scrollTop($('.chatcontainer')[0].scrollHeight); }, 500);
         $('#chattextinput').val('');
-        
     }
 });
 
 $('.minicog').on('click', function(e){
     settingsopen = true;
-    $(document.body).append(`<div class="settingscontainer"> <div class="settingswindow"> <div class="settingssidebar"> <p class="settingcategorytitle">user settings</p> <input type="button" value="My Account" class="settingbutton" style="color: white;background-color: rgba(77, 77, 77, 0.377);box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.274);cursor: pointer;"> <input type="button" value="Profiles" class="settingbutton"> <p class="settingseparator">-</p> <p class="settingcategorytitle">app settings</p> <input type="button" value="Appearence" class="settingbutton"> <p class="settingseparator">-</p> <p class="settingcategorytitle">Activity</p> <input type="button" value="What's New" class="settingbutton"> <input type="button" value="Log Out" class="settingbutton"> </div> </div> </div>`)
+    $(document.body).append(`<div class="settingscontainer"> <div class="settingswindow"> <div class="settingssidebar"> <p class="settingcategorytitle">user settings</p> <input type="button" settingname="myaccount" value="My Account" class="settingbutton"> <input type="button" settingname="profiles" value="Profiles" class="settingbutton"> <p class="settingseparator">-</p> <p class="settingcategorytitle">app settings</p> <input type="button" settingname="appearence" value="Appearence" class="settingbutton"> <p class="settingseparator">-</p> <p class="settingcategorytitle">Activity</p> <input type="button" settingname="whatsnew" value="What's New" class="settingbutton"> <input type="button" settingname="logout" value="Log Out" class="settingbutton"> </div> <div class="settingwindowcontainer"> </div> </div> </div>`)
 })
+
+//setttings bs
+
+$(document).on('click','.settingbutton', function(e){
+    $(this).parent().children().removeAttr('selected');
+    $(this).attr('selected','')
+    $(".settingwindowcontainer").empty();
+    if($(this).attr('settingname') == 'appearence' && currentsettingmenu != 'appearence'){
+        currentsettingmenu = 'appearence'
+        $(".settingwindowcontainer").append(`<p class="settingtitle">Appearence</p> <div class="messagepreviewcontainer"> <div class="message"> <div class="userprofileimg"></div> <p class="username">vewu</p> <p class="time">15:00</p><br><br> <p class="messagetext">Look at me, I'm a beautiful butterfly<br>Fluttering in the moonlight 🙂</p> </div> </div> <p class="settingtitle">Colors</p> <p class="innersettingtitle">Color Accent</p> <input class="settingcolormeter" id="color" type="range" min="0" max="255" step="1" value="0"> <p class="innersettingtitle">Color Saturation</p> <input class="settingcolormeter" id="saturation" type="range" min="0" max="15" step="1" value="0"><br> <p class="settingtitle">Chat</p> <p class="innersettingtitle">Chat Font Scaling (experimental)</p> <input class="settingrangemeter" id="chatfontscale" type="range" min="12" max="25" step="1" value="0">`)
+        return;
+    }
+    currentsettingmenu = $(this).attr('settingname');
+    
+})
+
+$(document).on('input','#color.settingcolormeter', function () {
+    $(":root").get(0).style.setProperty("--accentcolor", `${$(this).val()}`);
+    $('.settingscontainer').attr('style','backdrop-filter:none;');
+})
+
+$(document).on('input','#saturation.settingcolormeter', function () {
+    $(":root").get(0).style.setProperty("--accentsaturation", `${$(this).val()}%`);
+    $('.settingscontainer').attr('style','backdrop-filter:none;');
+})
+
+$(document).on('input','#chatfontscale.settingrangemeter', function () {
+    $(":root").get(0).style.setProperty("--messegefontsize", `${$(this).val()}px`);
+})
+
+$(document).on('click','.settingcolormeter', function () {
+    $('.settingscontainer').attr('style','backdrop-filter: blur(50px);');
+})
+
+//setttings bs
+
+//image wizardry
+
+$('.imageupbutton').on('change', function() {
+    if ($('.imageupbutton').prop("files") && $('.imageupbutton').prop("files")[0]) {
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            console.log(reader.result)
+        }
+        reader.readAsDataURL($('.imageupbutton').prop("files")[0]);
+    }
+});
+
+//image wizardry
 
 function guidGenerator() {
     var S4 = function() {
